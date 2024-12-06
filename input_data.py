a0b46a6e7f3ebb33a3dbc15389d79d39e3d17c4f
Extracheesy <desportes.cedric@gmail.com>
2024-12-06 18:53:28 +0100

    colab

--- input_data.py
+++ input_data.py
@@ -58,24 +58,24 @@ start_date = "2020-01-01"
 end_date = None
 strategy_type = ["long"]
 
-"""
+
 trix_lengths = [5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47]
 trix_signal_lengths = [5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47]
 trix_signal_types = ["ema", "sma"]
 long_ma_lengths = [100, 200, 300, 400, 500, 600, 700]
-"""
+
 """
 trix_lengths = [7, 11, 15, 20, 30, 41, 45, 49]
 trix_signal_lengths = [7, 11, 15, 20, 30, 41, 45, 49]
 trix_signal_types = ["sma", "ema"]
 long_ma_lengths = [200, 300, 500, 700]
 """
-
+"""
 trix_lengths = [7, 49]
 trix_signal_lengths = [7]
 trix_signal_types = ["sma"]
 long_ma_lengths = [200]
-
+"""
 # Define the possible values for each parameter
 timeframes = ["1h", "2h", "4h"]
 trading_pairs = lst_coin
